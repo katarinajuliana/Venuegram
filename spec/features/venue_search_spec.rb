594f4a5ce461f85aae6660903175baa2b6ad28dc
Katarina Blagojevic <katblag@gmail.com>
2013-09-30 20:48:32 -0700

    added contexts to search spec

--- spec/features/venue_search_spec.rb
+++ spec/features/venue_search_spec.rb
@@ -1,21 +1,25 @@
 require "spec_helper"
 
 describe "the venue search process", :js => true do
-  it "reports results when no venues found" do
-    visit root_path
-    fill_in 'Latitude & Longitude', :with => '1,1'
-    fill_in 'Radius in meters', :with => '0'
-    click_button 'Find Venues!'
+  context "when no venues match search"
+    it "tells user No Venues Match" do
+      visit root_path
+      fill_in 'Latitude & Longitude', :with => '1,1'
+      fill_in 'Radius in meters', :with => '0'
+      click_button 'Find Venues!'
     
-    expect(page).to have_content("No Venues Match Your Search Criteria")
+      expect(page).to have_content("No Venues Match Your Search Criteria")
+    end
   end
   
-  xit "finds venues on map" do
-    visit root_path
-    fill_in 'Latitude & Longitude', :with => '37.80809019289311,-122.27061431370356'
-    fill_in 'Radius in meters', :with => '1000'
-    click_button 'Find Venues!'
+  context "when venues are foud" do
+    xit "finds venues on map" do
+      visit root_path
+      fill_in 'Latitude & Longitude', :with => '37.80809019289311,-122.27061431370356'
+      fill_in 'Radius in meters', :with => '1000'
+      click_button 'Find Venues!'
     
-    # how do I test for Google Maps markers??
+      # how do I test for Google Maps markers??
+    end
   end
 end
